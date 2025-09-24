7eafa2c4d5cb1f1bf16fd6bf1fd09041559b0fa6
NNope <2735712359@qq.com>
2025-09-24 09:16:58 +0800

    套用panel小模板 vtkManager初版

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 import panel as pn
+from fastapi.responses import RedirectResponse
 import vtk
 from panel.pane import VTK
 import utils
 import io
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-html_context = utils.Read_template()
 
 pn.extension('vtk')
 
@@ -17,35 +16,39 @@ options = ["cube", "sphere", "cone"]
 seldrop = pn.widgets.Select(name='选择几何体', options=options, value='cube')
 output = pn.widgets.StaticText(name="显示选项", value="")
 
-
 render_window = utils.Create_vtk_cube()
 vtk_pane = VTK(render_window)
 
-html_panel = html_context.replace(
-    '<div id="panel-app"></div>',
-    utils.To_html(seldrop)+utils.To_html(output)
+
+template = pn.template.FastListTemplate(
+    title="VTK SHOW",
+    sidebar=[seldrop, output],
+    main=[vtk_pane],
 )
 
+pn.serve(
+    template,
+    port=5006,
+    allow_websocket_origin=["127.0.0.1:5006", "localhost:5006"],  
+    show=True,
+    )
 
-panel_app = pn.Column(seldrop,output, vtk_pane)
+@pn.depends(seldrop.param.value,watch=True)
+async def update_vtk(value):
+    render_ = utils.Create_vtk_cube()
+    if value == "cube":
+        render_ = utils.Create_vtk_cube()
+        output.value = "显示立方体"
+    elif value == "sphere":
+        render_ = utils.Create_vtk_sphere()
+        output.value = "显示球体"
+    elif value == "cone":
+        render_ = utils.Create_vtk_cone()
+        output.value = "显示圆锥体"
+    if vtk_pane is not None : 
+        vtk_pane.object = render_
 
-@pn.depends(seldrop.param.value, watch=True)
-async def update_output(value):
-    output.value = f"You choose: {value}"
-    match value:
-        case "cube":
-            render_window = utils.Create_vtk_cube()
-        case "sphere":
-            render_window = utils.Create_vtk_sphere()
-        case "cone":
-            render_window = utils.Create_vtk_cone()
-    global vtk_pane
-    if vtk_pane is not None :vtk_pane.object = render_window
 
 @app.get("/panel", response_class=HTMLResponse)
 def serve_panel():
-    return HTMLResponse(utils.To_html(panel_app))
-
-@app.get("/html", response_class=HTMLResponse)
-def serve_html():
-    return HTMLResponse(html_panel)
+    return RedirectResponse(url="http://127.0.0.1:5006")
--- vtk_core.py
+++ vtk_core.py
@@ -1,1 +1,34 @@
 import vtk
+
+# vtk有时会资源复用爆错 ，所以用单例模式管理
+class VtkManager:
+    __instance = None 
+    __dicpool = {}
+    __initialized = False
+    
+    @classmethod
+    def Instance(cls):
+        if cls.__instance is None:
+            cls.__instance = VtkManager()
+        return cls.__instance
+    
+    
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super(VtkManager, cls).__new__(cls)
+        return cls.__instance
+    
+    def __init__(self):
+        if not self.__initialized:
+            self.__initialized = True
+            # 
+            self.init()
+            
+    def init(self):
+        # Initialize VTK related settings if needed
+        pass
+    
+    def __del__(self):
+        # Cleanup resources if needed
+        pass
+        
