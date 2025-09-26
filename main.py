9d93e655c805a38871293d0941e47c85b742e2f6
NNope <2735712359@qq.com>
2025-09-26 14:54:36 +0800

    小组件 背景颜色切换时优化

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ import vtkmodules
 import vtkmodules.vtkRenderingCore
 import utils
 import vtk_core
+import time
+import httpx
 
 app = FastAPI()
 
@@ -20,27 +22,19 @@ options = ["cube", "sphere", "cone"]
 
 
 render_window = vtk_core.VtkManager.Create_vtk('cube')
-vtk_pane = pn.pane.VTK(render_window,sizing_mode='stretch_both',height=600)
+vtk_pane = pn.pane.VTK(render_window,sizing_mode='stretch_both')
 
 seldrop = pn.widgets.Select(name='选择几何体', options=options, value='cube')
 output = pn.widgets.StaticText(name="显示选项", value="显示立方体")
-colorpicker = pn.widgets.ColorPicker(name='背景颜色', value='#99ef78')
+colorpicker = pn.widgets.ColorPicker(name='背景颜色', value='#99ef78',sizing_mode='stretch_width')
+buttonReC = pn.widgets.Button(name = '重置摄像机位置',sizing_mode='stretch_width')
+buttonflash = pn.widgets.Button(name = '刷新',sizing_mode='stretch_width')
+
 
 vtk_pane.get_renderer().SetBackground(utils.hex_to_rgb('#99ef78')) #type:ignore
 
 
-template = pn.template.FastListTemplate(
-    title="VTK SHOW",
-    sidebar=[seldrop,output,colorpicker],
-    main=[vtk_pane],
-)
 
-pn.serve(
-    template,
-    port=5006,
-    allow_websocket_origin=["127.0.0.1:5006", "localhost:5006"],  
-    show=False,
-    )
 
 @pn.depends(seldrop.param.value,watch=True)
 def update_vtk(value):
@@ -55,7 +49,23 @@ def update_vtk(value):
         output.value = "显示圆锥体"
     if vtk_pane is not None : 
         vtk_pane.object = render_
-        update_bgcolor(colorpicker.value)
+        vtk_pane.get_renderer().SetBackground(utils.hex_to_rgb(colorpicker.value)) #type:ignore
+        time.sleep(0.01) #两次更新之间需要等待一会 不然会无法显示
+        vtk_pane.param.trigger('object')
+        
+
+def reset_camera(event):
+    if vtk_pane is not None:
+        assert isinstance(vtk_pane, pn.pane.vtk.vtk.VTKRenderWindowSynchronized)
+        renderer = vtk_pane.get_renderer()
+        assert isinstance(renderer, vtkmodules.vtkRenderingCore.vtkRenderer)
+        renderer.ResetCamera()
+        vtk_pane.param.trigger('object')
+buttonReC.on_click(reset_camera)
+
+def flash(event):
+        vtk_pane.param.trigger('object') #type:ignore
+buttonflash.on_click(flash)
 
 
 @pn.depends(colorpicker.param.value,watch=True)
@@ -66,6 +76,20 @@ def update_bgcolor(value):
     render.SetBackground(utils.hex_to_rgb(value))
     vtk_pane.param.trigger('object') #显式更新
 
-@app.get("/panel", response_class=HTMLResponse)
+
+template = pn.template.FastListTemplate(
+    title="VTK SHOW",
+    sidebar=[seldrop,output,colorpicker,buttonReC,buttonflash],
+    main=[vtk_pane],
+)
+
+pn.serve(
+    template,
+    port=5006,
+    allow_websocket_origin=["127.0.0.1:5006", "localhost:5006"],  
+    show=False,
+    )
+
+@app.get("/panel")
 def serve_panel():
-    return RedirectResponse(url="http://127.0.0.1:5006")
+    return RedirectResponse(url="http://127.0.0.1:5006")\
